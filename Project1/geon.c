247f3d238132c4d53feddb4fd24c1ce8a9633686
kop <ak1ak785@naver.com>
2019-06-27 11:30:54 +0900

    scanf 오류 해결 및 최종 출력 안되는 것 해결

--- Project1/geon.c
+++ Project1/geon.c
@@ -7,11 +7,12 @@ void main() {
 	int sum = 0;
 	
 	printf("두 개의 숫자를 입력하시오 : ");
-	scanf("%d %d", &a, &b);
+	scanf_s("%d %d", &a, &b);
 
 	sum = a + b;
 
 	printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
 
+	system("pause");
 	return 0;
 }
