f83c6186ec0bf79d090607ef1e7821f6bdeb1575
kop <ak1ak785@naver.com>
2019-06-27 11:39:07 +0900

    뺼셈 추가 하였습니다.(오류 없음)

--- Project1/geon.c
+++ Project1/geon.c
@@ -5,13 +5,16 @@ void main() {
 	
 	int a, b;
 	int sum = 0;
-	
+	int sub = 0;
+
 	printf("두 개의 숫자를 입력하시오 : ");
 	scanf_s("%d %d", &a, &b);
 
 	sum = a + b;
+	sub = a - b;
 
 	printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
+	printf("입력하신 두 개의 숫자의 차는 %d 입니다.\n", sub);
 
 	system("pause");
 	return 0;
