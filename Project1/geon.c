731b07da181be49990749779903c66ca603f45e2
kop <ak1ak785@naver.com>
2019-06-27 11:43:53 +0900

    나누기 추가 하였습니다.(오류 x)

--- Project1/geon.c
+++ Project1/geon.c
@@ -7,7 +7,7 @@ void main() {
 	int sum = 0;
 	int sub = 0;
 	int mul = 0;
-
+	int div = 0;
 
 	printf("두 개의 숫자를 입력하시오 : ");
 	scanf_s("%d %d", &a, &b);
@@ -15,10 +15,12 @@ void main() {
 	sum = a + b;
 	sub = a - b;
 	mul = a * b;
+	div = a / b;
 
 	printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
 	printf("입력하신 두 개의 숫자의 차는 %d 입니다.\n", sub);
 	printf("입력하신 두 개의 숫자의 곱은 %d 입니다.\n", mul);
+	printf("입력하신 두 개의 숫자의 나눔은 %d 입니다.\n", div);
 
 	system("pause");
 	return 0;
