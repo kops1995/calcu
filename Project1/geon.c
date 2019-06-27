258ef0027af73bc083df9ba1821546ca4a1a8d13
kop <ak1ak785@naver.com>
2019-06-27 13:00:44 +0900

    반복문을 추가하여 5번 반복되게 해 놓았습니다.

--- Project1/geon.c
+++ Project1/geon.c
@@ -3,30 +3,32 @@
 
 void main() {
 	
-	int a, b;
+	int a, b, i;
 	int sum = 0;
 	int sub = 0;
 	int mul = 0;
 	int div = 0;
 	int rem = 0;
 
-	printf("두 개의 숫자를 입력하시오 : ");
-	scanf_s("%d %d", &a, &b);
+	for (i = 0; i < 5; i++) {
+		printf("두 개의 숫자를 입력하시오 : ");
+		scanf_s("%d %d", &a, &b);
 
-	sum = a + b;
-	sub = a - b;
-	mul = a * b;
-	div = a / b;
-	rem = a % b;
+		sum = a + b;
+		sub = a - b;
+		mul = a * b;
+		div = a / b;
+		rem = a % b;
 
-	printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
-	printf("입력하신 두 개의 숫자의 차는 %d 입니다.\n", sub);
-	printf("입력하신 두 개의 숫자의 곱은 %d 입니다.\n", mul);
-	if (a > b) {
-		printf("입력하신 두 개의 숫자의 몫은 %d 입니다.\n", div);
+		printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
+		printf("입력하신 두 개의 숫자의 차는 %d 입니다.\n", sub);
+		printf("입력하신 두 개의 숫자의 곱은 %d 입니다.\n", mul);
+		if (a > b) {
+			printf("입력하신 두 개의 숫자의 몫은 %d 입니다.\n", div);
+		}
+		printf("입력하신 두 개의 숫자의 나머지는 %d 입니다.\n", rem);
+
 	}
-	printf("입력하신 두 개의 숫자의 나머지는 %d 입니다.\n", rem);
-
 	system("pause");
 	return 0;
 }
