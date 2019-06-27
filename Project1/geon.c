ebdf82890b899808fe5a1b3312b6188c72d6f5da
kop <ak1ak785@naver.com>
2019-06-27 14:01:40 +0900

    주석 부분 삭제, 계산할 두 개의 숫자 입력 부분 다시 switch문 안으로 이동, 일정시간 조건 뒤에 화면 갱신

--- Project1/geon.c
+++ Project1/geon.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 void main() {
 	
@@ -11,42 +12,63 @@ void main() {
 	int rem = 0;
 	
 	while (1) {
-	printf("두 개의 숫자를 입력하시오. ");
-	scanf_s("%d %d", &a, &b);
 
 	printf("1. 덧셈 2. 뺄셈 3. 곱셈 4. 나눗셈(몫) 5. 나눗셈(나머지) 6. 프로그램 종료\n");
 	printf("----------------------------------------------------------------\n");
 	printf("원하는 연산을 선택하시오. ");
 	scanf_s("%d", &c);
-	
 
-
-		switch (c)
+		switch(c)
 		{
 		case 1:
+			printf("두 개의 숫자를 입력하시오. ");
+			scanf_s("%d %d", &a, &b);
+
 			sum = a + b;
 
 			printf("덧셈의 결과는 %d 입니다.\n", sum);
+			Sleep(1500);
+			system("cls");
 			break;
 		case 2:
+			printf("두 개의 숫자를 입력하시오. ");
+			scanf_s("%d %d", &a, &b);
+
 			sub = a - b;
 
 			printf("뺄셈의 결과는 %d 입니다.\n", sub);
+			Sleep(1500);
+			system("cls");
 			break;
 		case 3:
+			printf("두 개의 숫자를 입력하시오. ");
+			scanf_s("%d %d", &a, &b);
+
 			mul = a * b;
 
 			printf("곱셈의 결과는 %d 입니다.\n", mul);
+			Sleep(1500);
+			system("cls");
 			break;
 		case 4:
+			printf("두 개의 숫자를 입력하시오. ");
+			scanf_s("%d %d", &a, &b);
+
 			div = a / b;
 
 			printf("나눗셈(몫)의 결과는 %d 입니다.\n", div);
+			Sleep(1500);
+			system("cls");
 			break;
 		case 5:
+			printf("두 개의 숫자를 입력하시오. ");
+			scanf_s("%d %d", &a, &b);
+
 			rem = a % b;
 
 			printf("나눗셈(나머지)의 결과는 %d 입니다.\n", rem);
+			Sleep(1500);
+			system("cls");
 			break;
 		case 6:
 			system("pause");
@@ -58,24 +80,4 @@ void main() {
 			break;
 		}
 	}
-	//add 브랜치의 내용
-	/*printf("두 개의 숫자를 입력하시오 : ");
-	scanf_s("%d %d", &a, &b);
-
-	sum = a + b;
-	sub = a - b;
-	mul = a * b;
-	div = a / b;
-	rem = a % b;
-
-	printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
-	printf("입력하신 두 개의 숫자의 차는 %d 입니다.\n", sub);
-	printf("입력하신 두 개의 숫자의 곱은 %d 입니다.\n", mul);
-	if (a > b) {
-		printf("입력하신 두 개의 숫자의 몫은 %d 입니다.\n", div);
-	}
-	printf("입력하신 두 개의 숫자의 나머지는 %d 입니다.\n", rem);
-	*/
-
-	
 }
