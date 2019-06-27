55bb3b828a0de2f8c6ced69cfbb278ecd622e46a
kop <ak1ak785@naver.com>
2019-06-27 11:41:22 +0900

    곱셈을 추가하였습니다. (오류 없음)

--- Project1/geon.c
+++ Project1/geon.c
@@ -6,15 +6,19 @@ void main() {
 	int a, b;
 	int sum = 0;
 	int sub = 0;
+	int mul = 0;
+
 
 	printf("두 개의 숫자를 입력하시오 : ");
 	scanf_s("%d %d", &a, &b);
 
 	sum = a + b;
 	sub = a - b;
+	mul = a * b;
 
 	printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
 	printf("입력하신 두 개의 숫자의 차는 %d 입니다.\n", sub);
+	printf("입력하신 두 개의 숫자의 곱은 %d 입니다.\n", mul);
 
 	system("pause");
 	return 0;
