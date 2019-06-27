75f79eb0dbe5c0a405d1b70a321b23dea4a39dbe
kop <ak1ak785@naver.com>
2019-06-27 11:53:39 +0900

    나머지 연산과 나눔->몫으로 변경

--- Project1/geon.c
+++ Project1/geon.c
@@ -8,6 +8,7 @@ void main() {
 	int sub = 0;
 	int mul = 0;
 	int div = 0;
+	int rem = 0;
 
 	printf("두 개의 숫자를 입력하시오 : ");
 	scanf_s("%d %d", &a, &b);
@@ -16,11 +17,13 @@ void main() {
 	sub = a - b;
 	mul = a * b;
 	div = a / b;
+	rem = a % b;
 
 	printf("입력하신 두 개의 숫자의 합은 %d 입니다.\n", sum);
 	printf("입력하신 두 개의 숫자의 차는 %d 입니다.\n", sub);
 	printf("입력하신 두 개의 숫자의 곱은 %d 입니다.\n", mul);
-	printf("입력하신 두 개의 숫자의 나눔은 %d 입니다.\n", div);
+	printf("입력하신 두 개의 숫자의 몫은 %d 입니다.\n", div);
+	printf("입력하신 두 개의 숫자의 나머지는 %d 입니다.\n", rem);
 
 	system("pause");
 	return 0;
